c0c0a53292b2998b32007e64876371715d82bd8c
rodrigo21904445 <61628391+rodrigo21904445@users.noreply.github.com>
2022-12-12 01:01:42 +0000

    Add files via upload

---
+++ v2.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "path/filepath"
+  "sync"
+  "strings"
+)
+
+func renameFilesAndFolders(path string, oldStr string, newStr string) error {
+  newPath := strings.Replace(path, oldStr, newStr, -1)
+  return os.Rename(path, newPath)
+}
+
+func main () {
+
+  var wg sync.WaitGroup
+
+  root := "./tree"
+  oldStr := "f"
+  newStr := "g"
+  slice := make([]string, 0)
+
+  filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+    if err != nil {
+      fmt.Println(err)
+      return err
+    }
+
+    wg.Add(1)
+    go func() {
+      slice = append(slice, path)
+      wg.Done()
+      }()
+
+    return nil
+  })
+
+  wg.Wait()
+
+  if len(slice) > 0 {
+    for i := len(slice) - 1; i >= 0; i-- {
+      renameFilesAndFolders(slice[i], oldStr, newStr)
+    }
+  }
+}
